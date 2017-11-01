18ef855090328e90db726f4e504880df139eef05
rodolfo-gonzalez4335 <rodolfo.gonzalez4335@gmail.com>
2017-11-01 01:49:29 -0500

    First Commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rudy/simple_webb_app/django_project/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ helloapp/howdy/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+ # howdy/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
+# Create your views here.
+
+# Add this view
+class AboutPageView(TemplateView):
+    template_name = "about.html"
